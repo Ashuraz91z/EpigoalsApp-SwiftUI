cceef4de2ec2aa67ee9d100ca45d26a0165955bd
Lucas Fernandes <lucas.epitech@epitech.digital>
2024-08-09 07:31:36 +0200

    Initial Commit

---
+++ Epigoals/EpigoalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpigoalsApp.swift
+//  Epigoals
+//
+//  Created by Lucas Fernandes on 09/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct EpigoalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
